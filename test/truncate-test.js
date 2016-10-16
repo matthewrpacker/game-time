f8ce11c1139c215ff668fc37e7c1746ed59e5991
Kris Sparks <kbs5280@gmail.com>
2016-10-16 16:09:47 -0600

    Unit test functions

---
+++ test/truncate-test.js
@@ -0,0 +1,13 @@
+const chai = require('chai')
+const expect = chai.expect;
+
+const truncate = require('../lib/truncate')
+
+describe ('Truncate', function() {
+  context('truncateNumber', function() {
+    it('should return a truncated number', function() {
+      result = truncate.truncateNumber(10.99)
+      expect(result).to.eq(10)
+    })
+  })
+})
