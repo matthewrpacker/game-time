cebb4245b7168fb352e7733419e102a70ca3f0b1
Kris Sparks <kbs5280@gmail.com>
2016-10-19 17:15:47 -0600

    0 issues

--- test/ufo-test.js
+++ test/ufo-test.js
@@ -1,51 +1,51 @@
-const chai = require('chai')
+const chai = require('chai');
 const expect = chai.expect;
 
-const game = require('../lib/game')
-const Ufo = require('../lib/ufo')
+const game = require('../lib/game');
+const Ufo = require('../lib/ufo');
 
 describe ('Ufo', function() {
   context('default behavior', function() {
     it('should be instantiated', function() {
       function test() {
-        let ufo = new Ufo;
-        expect(ufo).to.be.an('object')
+        let ufo = new Ufo();
+        expect(ufo).to.be.an('object');
       }
-    })
+    });
     it('should have an x-coordinate', function() {
       function test() {
         let ufo = new Ufo(1);
-        expect(ufo.x).to.deep.equal(1)
+        expect(ufo.x).to.deep.equal(1);
       }
-    })
+    });
     it('should have a y-coordinate', function() {
       function test() {
         let ufo = new Ufo(1, 2);
-        expect(ufo.y).to.deep.equal(2)
+        expect(ufo.y).to.deep.equal(2);
       }
-    })
+    });
     it('should have a width', function() {
       function test() {
         let ufo = new Ufo(1, 2, 3);
-        expect(ufo.width).to.deep.equal(3)
+        expect(ufo.width).to.deep.equal(3);
       }
-    })
+    });
     it('should have a height', function() {
       function test() {
         let ufo = new Ufo(1, 2, 3, 4);
-        expect(ufo.height).to.deep.equal(4)
+        expect(ufo.height).to.deep.equal(4);
       }
-    })
+    });
     it('should have a boolean value', function() {
       function test() {
         let ufo = new Ufo(1, 2, 3, 4, true);
-        expect(ufo.isFlying).to.deep.equal(true)
+        expect(ufo.isFlying).to.deep.equal(true);
         let ufo1 = new Ufo(1, 2, 3, 4, false);
-        expect(ufo1.isFlying).to.deep.equal(false)
+        expect(ufo1.isFlying).to.deep.equal(false);
       }
-    })
-  })
-})
+    });
+  });
+});
 
 describe ('checkFlight', function() {
   context('is in flight', function() {
@@ -53,17 +53,17 @@ describe ('checkFlight', function() {
       function test() {
         let ufo = new Ufo(60, 60, 10, 10, true);
         ufo.checkFlight();
-        expect(nextBlockY).to.eq(ufo.y - 3)
+        expect(nextBlockY).to.eq(ufo.y - 3);
       }
-    })
-  })
+    });
+  });
   context('is not in flight', function() {
     it('should return false', function() {
       function test() {
         let ufo = new Ufo(60, 60, 10, 10, false);
         ufo.checkFlight();
-        expect(nextBlockY).to.eq(ufo.y + 3)
+        expect(nextBlockY).to.eq(ufo.y + 3);
       }
-    })
-  })
-})
+    });
+  });
+});
